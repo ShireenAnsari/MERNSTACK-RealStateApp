58719a9eaa65c82c3beb45c7edd13a3c6a48cf43
shiri <30csstudent@gmail.com>
2024-03-22 22:38:47 +0500

    createdpages+route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,20 @@
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import About from './pages/About'
+import Profile from './pages/Profile'
 const App = () => {
   return (
-    <div className="bg-black text-white">App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/sign-in' element={<Signin/>}/>
+      <Route path='/sign-up' element={<Signup/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
